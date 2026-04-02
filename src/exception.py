217b2d1983b16e46e11335d30c09d050fec40f94
Justinfi.info <Justinfi.info@gmail.com>
2026-04-02 23:50:59 +0530

    Save progress before rebase

--- src/exception.py
+++ src/exception.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import sys
 import logging
 
@@ -26,3 +27,43 @@ if __name__ == "__main__":
     except Exception as e:
         logging.info("Divide by zero error occurred.")
         raise CustomException(e, sys)
+=======
+import sys
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+def error_message_detail(error, error_detail):
+    _, _, exc_tb = error_detail.exc_info()
+
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+    else:
+        file_name = "Unknown"
+        line_number = "Unknown"
+
+    error_message = (
+        f"Error occurred in python script name [{file_name}], "
+        f"line number [{line_number}], error message [{str(error)}]"
+    )
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Divide by zero error occurred.")
+        raise CustomException(e, sys)
+>>>>>>> 32804fe (Save progress before rebase)
