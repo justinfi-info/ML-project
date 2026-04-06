2ec3e6de8aba588e6e866fbc4f3bd5b0e9e9b24e
Justinfi.info <Justinfi.info@gmail.com>
2026-04-06 18:38:31 +0530

    Update

--- pipeline/predict_pipeline.py
+++ pipeline/predict_pipeline.py
@@ -22,37 +22,41 @@ class PredictPipeline:
             raise CustomException(e, sys)
 
 
-Class CustomData:
-    def __init__(self,
+class CustomData:
+    def __init__(
+        self,
         gender: str,
-        ethnicity: str,
+        race_ethnicity: str,
         parental_level_of_education: str,
         lunch: str,
         test_preparation_course: str,
-        reading_score: int,
-        writing_score: int
+        reading_score: float,
+        writing_score: float,
     ):
         self.gender = gender
-        self.ethnicity = ethnicity
+        self.race_ethnicity = race_ethnicity
         self.parental_level_of_education = parental_level_of_education
         self.lunch = lunch
         self.test_preparation_course = test_preparation_course
         self.reading_score = reading_score
         self.writing_score = writing_score
 
-    def get_data_as_dataframe(self):
+    def to_dataframe(self) -> pd.DataFrame:
         try:
             custom_data_input_dict = {
                 "gender": [self.gender],
-                "race/ethnicity": [self.ethnicity],
+                "race_ethnicity": [self.race_ethnicity],
                 "parental_level_of_education": [self.parental_level_of_education],
                 "lunch": [self.lunch],
                 "test_preparation_course": [self.test_preparation_course],
                 "reading_score": [self.reading_score],
-                "writing_score": [self.writing_score]
+                "writing_score": [self.writing_score],
             }
 
             return pd.DataFrame(custom_data_input_dict)
 
         except Exception as e:
             raise CustomException(e, sys)
+
+    def get_data_as_dataframe(self) -> pd.DataFrame:
+        return self.to_dataframe()
